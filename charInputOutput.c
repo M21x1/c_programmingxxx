7cfe95a5ef8069bd0c6239b003d5507743c034de
Mjcv <max_83_14@hotmail.com>
2024-03-31 22:03:45 -0500

    onlycharnputoutput

---
+++ charInputOutput.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    char c;
+    printf("Enter some character. Enter $ to exit...\n");
+    while(c != '$')
+    {
+        c = getchar(); /* fgets it gets a line, getchar gets just a char */
+        printf("\n Entered character is: ");
+        putchar(c);
+        printf("\n");
+    }
+    return 0;
+
+}
